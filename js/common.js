64593ddd03104c0b6c5d16643808fc741c683c23
Georgiy <goshazvir@gmail.com>
2014-03-21 17:06:30 +0200

    sticky footer

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+	$(function() {
+		var footerHeight = $(".footer").height();
+		$(".out").css("margin-bottom", -footerHeight);
+		$(".push").css("height", footerHeight);
+	});
 
+	$(window).resize(function() {
+		var footerHeight = $(".footer").height();
+		$(".out").css("margin-bottom", -footerHeight);
+		$(".push").css("height", footerHeight);
+	});
 });
